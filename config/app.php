7ab6bb7c550846029d19ddf765a3df0882882783
Mr.Rabbit <a@eloli.moe>
2017-03-12 14:41:47 +1300

    增加验证码

--- config/app.php
+++ config/app.php
@@ -155,7 +155,10 @@ return [
         App\Providers\AuthServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /*
+         * GeekTest
+         */
+        Germey\Geetest\GeetestServiceProvider::class,
     ],
 
     /*
@@ -201,6 +204,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Geetest' => Germey\Geetest\Geetest::class,
 
     ],
 
